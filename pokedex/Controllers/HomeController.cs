0e24b341c70cf5c11028f5ab04b114ceeca2c99c
mandinha2108 <amafur8@gmail.com>
2024-03-25 11:33:53 -0300

    pagina 158

--- pokedex/Controllers/HomeController.cs
+++ pokedex/Controllers/HomeController.cs
@@ -19,36 +19,16 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         List<Pokemon> pokemons = GetPokemons();
-        using (StreamReader leitor = new("Data\\pokemons.json"))
-        {
-            string dados = leitor.ReadToEnd();
-            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
-        }
-        List<Tipo> tipos = [];
-        using (StreamReader leitor = new("Data\\tipos.json"))
-        {
-            string dados = leitor.ReadToEnd();
-            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
-        }
+        List<Tipo> tipos = GetTipos();
         ViewData["Tipos"] = tipos;
         return View(pokemons);
     }
 
     public IActionResult Details(int id)
     {
-        List<Pokemon> pokemons = [];
-        using (StreamReader leitor = new("Data\\pokemons.json"))
-        {
-            string dados = leitor.ReadToEnd();
-            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
-        }
-        List<Tipo> tipos= [];
-        using (StreamReader leitor = new("Data\\tipos.json"))
-        {
-            string dados = leitor.ReadToEnd(); 
-        tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
-        }
-    DetailsVM details = new(){
+        List<Pokemon> pokemons = GetPokemons();
+        List<Tipo> tipos= GetTipos();
+        DetailsVM details = new() {
         Tipos = tipos,
         Atual = pokemons.FirstOrDefault(p => p.Numero == id),
         Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
--- pokedex/wwwroot/js/site.js
+++ pokedex/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function filter(type) {
+    let cards, i;
+    let count = 0;
+    cards = document.getElementByClsssName("card");
+    buttons = document.getElementsByClassName("btn-filter");
+    for  (i=0; i < cards.lenght; i++) {
+        cards[i].parentElement.style.display = 'none';
+        if (cards[i].classList.contains(type) || type === "all" ) {
+            cards[i].parentElement.style.display = 'block';
+            count += 1;
+        };
+    };
+    for (i = 0; i < buttons.lenght; i++) {
+        if (buttons[i].id == `btn-${type}`) {
+            buttons[i].classList.remove("btn-sm");
+            buttons[i].classList.add(btn-md);
+        }
+        else {
+            buttons[i].classList.remove("btn-md");
+            buttons[i].classList.add("btn-sm");
+        }
+    };
+    if (type === "all") {
+        document.getElementById("btn-md");
+        document.getElementById("btn-sm");
+    };
+    if (count == 0) 
+        document.getElementById("zeroPokemon").classList.remove("d-none");
+    else 
+        document.getElementById("zeroPokemon").classList.add("d-none");
+}
