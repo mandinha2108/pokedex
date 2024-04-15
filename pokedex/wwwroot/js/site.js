a5e879b24449258598822a4e7014c8b6c3b63d7e
mandinha2108 <amafur8@gmail.com>
2024-04-15 11:01:31 -0300

    FINALIZADOOOOOOOOOOOOOOOOOO

--- pokedex/wwwroot/js/site.js
+++ pokedex/wwwroot/js/site.js
@@ -1,16 +1,16 @@
 ﻿function filter(type) {
     let cards, i;
     let count = 0;
-    cards = document.getElementByClsssName("card");
+    cards = document.getElementsByClassName("card");
     buttons = document.getElementsByClassName("btn-filter");
-    for  (i=0; i < cards.lenght; i++) {
+    for (i = 0; i < cards.length; i++) {
         cards[i].parentElement.style.display = 'none';
         if (cards[i].classList.contains(type) || type === "all" ) {
             cards[i].parentElement.style.display = 'block';
             count += 1;
         };
     };
-    for (i = 0; i < buttons.lenght; i++) {
+    for (i = 0; i < buttons.length; i++) {
         if (buttons[i].id == `btn-${type}`) {
             buttons[i].classList.remove("btn-sm");
             buttons[i].classList.add(btn-md);
